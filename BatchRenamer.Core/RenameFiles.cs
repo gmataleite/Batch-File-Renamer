63a67c8df70d62f4eec4c23f3d565ae69512ea46
Guilherme Leite <guilherme.leite.dev@gmail.com>
2026-02-27 00:18:48 -0300

    refactor: rename FileLogic to RenameFiles to improve naming consistency

--- BatchRenamer.Core/RenameFiles.cs
+++ BatchRenamer.Core/RenameFiles.cs
@@ -2,14 +2,22 @@
 
 namespace BatchRenamer.Core;
 
-public static class FileLogic
+public static class RenameFiles
 {
     public static string GenerateNewName(string oldName, string searchedText, string replacedtext = "")
     {
         string oldNameWithoutExtension = Path.GetFileNameWithoutExtension(oldName);
         string extensionFile = Path.GetExtension(oldName);
 
-        string newName = oldNameWithoutExtension.Replace(searchedText, replacedtext) + extensionFile;
+        string newName;
+
+        if (searchedText == "")
+        {
+            newName = oldName;
+        }
+        else {
+            newName = oldNameWithoutExtension.Replace(searchedText, replacedtext) + extensionFile;
+        }
 
         return newName;
     }
